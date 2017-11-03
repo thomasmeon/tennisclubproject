ec36f63500779a4722045bd82d63d2e4b32bf1ed
CorentinDore <CorentinDore>
2017-11-03 18:34:31 +0100

    update Booking Class

--- tennisclub/src/main/java/com/frenchies/tennisclub/entity/Booking.java
+++ tennisclub/src/main/java/com/frenchies/tennisclub/entity/Booking.java
@@ -1,16 +1,12 @@
 package com.frenchies.tennisclub.entity;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -21,42 +17,50 @@ import com.frenchies.tennisclub.enums.Hour24;
 //@Author Dore Corentin UCO 473308
 
 @Entity
-@Table(name="BOOKING")
+@Table(name = "BOOKING")
 public class Booking {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idBooking;
-	
-	@ManyToOne(optional=false)
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idCourt;
+
+	@ManyToOne(optional = false)
 	@NotNull
 	private People player1;
-	
-	@ManyToOne(optional=false)
+
+	@ManyToOne(optional = false)
 	@NotNull
 	private People player2;
-	
-	@OneToMany
-	@NotNull
-	private List<Court> court = new ArrayList<Court>();
-	
+
 	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date dateOfBooking;
-	
+
 	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
 	private Hour24 hourOfBooking;
-	
 
-	public Booking(Long idBooking,People player1, People player2, Date dateOfBooking, Hour24 hourOfBooking ) {
-		this.idBooking=idBooking;
-		this.player1=player1;
-		this.player2=player2;
-		this.dateOfBooking=dateOfBooking;
-		this.hourOfBooking=hourOfBooking;
-		
+	public Booking(Long idBooking, Long idCourt, People player1, People player2, Date dateOfBooking,
+			Hour24 hourOfBooking) {
+		this.idBooking = idBooking;
+		this.idCourt = idCourt;
+		this.player1 = player1;
+		this.player2 = player2;
+		this.dateOfBooking = dateOfBooking;
+		this.hourOfBooking = hourOfBooking;
+
 	}
-	
+
+	public Long getIdCourt() {
+		return idCourt;
+	}
+
+	public void setIdCourt(Long idCourt) {
+		this.idCourt = idCourt;
+	}
 
 	public People getPlayer1() {
 		return player1;
@@ -73,15 +77,6 @@ public class Booking {
 	public void setPlayer2(People player2) {
 		this.player2 = player2;
 	}
-	
-	public List<Court> getCourt() {
-		return Collections.unmodifiableList(court);
-	}
-
-	public void addBookingItem(Court p) {
-		court.add(p);
-	}
-
 
 	public Date getDateOfBooking() {
 		return dateOfBooking;
@@ -90,7 +85,7 @@ public class Booking {
 	public void setDateOfBooking(Date dateOfBooking) {
 		this.dateOfBooking = dateOfBooking;
 	}
-	
+
 	public Hour24 getHourOfBooking() {
 		return hourOfBooking;
 	}
@@ -98,19 +93,23 @@ public class Booking {
 	public void setHourOfBooking(Hour24 hourOfBooking) {
 		this.hourOfBooking = hourOfBooking;
 	}
-	
+
 	public Long getIdBooking() {
 		return idBooking;
+	}
+
+	public void setIdBooking(Long idBooking) {
+		this.idBooking = idBooking;
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((court == null) ? 0 : court.hashCode());
 		result = prime * result + ((dateOfBooking == null) ? 0 : dateOfBooking.hashCode());
 		result = prime * result + ((hourOfBooking == null) ? 0 : hourOfBooking.hashCode());
 		result = prime * result + ((idBooking == null) ? 0 : idBooking.hashCode());
+		result = prime * result + ((idCourt == null) ? 0 : idCourt.hashCode());
 		result = prime * result + ((player1 == null) ? 0 : player1.hashCode());
 		result = prime * result + ((player2 == null) ? 0 : player2.hashCode());
 		return result;
@@ -125,11 +124,6 @@ public class Booking {
 		if (getClass() != obj.getClass())
 			return false;
 		Booking other = (Booking) obj;
-		if (court == null) {
-			if (other.court != null)
-				return false;
-		} else if (!court.equals(other.court))
-			return false;
 		if (dateOfBooking == null) {
 			if (other.dateOfBooking != null)
 				return false;
@@ -141,6 +135,11 @@ public class Booking {
 			if (other.idBooking != null)
 				return false;
 		} else if (!idBooking.equals(other.idBooking))
+			return false;
+		if (idCourt == null) {
+			if (other.idCourt != null)
+				return false;
+		} else if (!idCourt.equals(other.idCourt))
 			return false;
 		if (player1 == null) {
 			if (other.player1 != null)
@@ -154,9 +153,5 @@ public class Booking {
 			return false;
 		return true;
 	}
-	
 
-
-	
-	
 }
