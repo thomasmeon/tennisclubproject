1ebd1e6a3d523a52bab7485e3041a9c0aa432f70
CorentinDore <CorentinDore>
2017-11-26 20:47:47 +0100

    Test_CourtServiceTest_Validate

--- tennisclub-service/src/test/java/com/frenchies/tennisclub/service/CourtServiceTest.java
+++ tennisclub-service/src/test/java/com/frenchies/tennisclub/service/CourtServiceTest.java
@@ -2,7 +2,6 @@ package com.frenchies.tennisclub.service;
 
 import org.hibernate.service.spi.ServiceException;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -12,17 +11,16 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
 import org.springframework.transaction.annotation.Transactional;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.frenchies.tennisclub.dao.CourtDao;
 import com.frenchies.tennisclub.entity.Court;
 import com.frenchies.tennisclub.enums.CourtType;
+import com.frenchies.tennisclub.enums.Status;
 import com.frenchies.tennisclub.service.config.ServiceConfiguration;
 
-
-
 /**
- * Basic tests for facade implementations using a mock of the service layer.
+ * Basic tests for service implementations
  *
  * @author CorentinDore 473308
  */
@@ -31,21 +29,28 @@ import com.frenchies.tennisclub.service.config.ServiceConfiguration;
 @ContextConfiguration(classes = ServiceConfiguration.class)
 @Transactional
 public class CourtServiceTest extends AbstractTestNGSpringContextTests {
-	@Mock
-	private CourtDao courtDao;
 
 	@Autowired
 	@InjectMocks
 	private CourtService courtService;
+	private Court c;
+	protected CourtType newCourtType;
 
 	@BeforeClass
 	public void setup() throws ServiceException {
 		MockitoAnnotations.initMocks(this);
 	}
 
+	@BeforeMethod
+	public void init() {
+		c = new Court(Status.AVAILABLE, CourtType.CARPET, 234, 34);
+		courtService.createCourt(c);
+
+		newCourtType = (CourtType.GRASS);
+	}
 
 	@Test
-	public void changeType(Court c, CourtType newCourtType) {
+	public void changeCourtType() {
 
 		courtService.changeCourtType(c, newCourtType);
 
