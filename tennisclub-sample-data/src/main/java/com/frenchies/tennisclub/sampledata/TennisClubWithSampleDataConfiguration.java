93f308c994648aea8174d1faadb567b5a4467c61
Meon <thomasmeon@hotmail.fr>
2017-12-12 15:53:19 +0100

    debug testSampleData

--- tennisclub-sample-data/src/main/java/com/frenchies/tennisclub/sampledata/TennisClubWithSampleDataConfiguration.java
+++ tennisclub-sample-data/src/main/java/com/frenchies/tennisclub/sampledata/TennisClubWithSampleDataConfiguration.java
@@ -1,9 +1,15 @@
 package com.frenchies.tennisclub.sampledata;
 
 import com.frenchies.tennisclub.service.config.ServiceConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 
 /**
  * Takes ServiceConfiguration and adds the SampleDataLoadingFacade bean.
@@ -13,7 +19,16 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import(ServiceConfiguration.class)
 @ComponentScan(basePackageClasses = {SampleDataLoadingFacadeImpl.class})
-
 public class TennisClubWithSampleDataConfiguration {
+	
+	final static Logger log = LoggerFactory.getLogger(TennisClubWithSampleDataConfiguration.class);
 
+    @Autowired
+    SampleDataLoadingFacade sampleDataLoadingFacade;
+
+    @PostConstruct
+    public void dataLoading() throws IOException {
+        log.debug("dataLoading()");
+        sampleDataLoadingFacade.loadData();
+    }
 }
--- tennisclub-sample-data/src/test/java/com/frenchies/tennisclub/sampledata/SampleDataLoadingFacadeTest.java
+++ tennisclub-sample-data/src/test/java/com/frenchies/tennisclub/sampledata/SampleDataLoadingFacadeTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.frenchies.tennisclub.sampledata;
 
 import java.io.IOException;
@@ -19,6 +18,7 @@ import org.testng.annotations.Test;
 
 import com.frenchies.tennisclub.dao.BookingDao;
 import com.frenchies.tennisclub.dao.CourtDao;
+import com.frenchies.tennisclub.dao.UserDao;
 import com.frenchies.tennisclub.entity.User;
 import com.frenchies.tennisclub.service.UserService;
 
@@ -28,7 +28,9 @@ import com.frenchies.tennisclub.service.UserService;
  * @author Meon Thomas 473449
  */
 @ContextConfiguration(classes = {TennisClubWithSampleDataConfiguration.class})
-public class SampleDataLoadingFacadeTest {
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class SampleDataLoadingFacadeTest extends AbstractTestNGSpringContextTests {
 
     final static Logger log = LoggerFactory.getLogger(SampleDataLoadingFacadeTest.class);
 
@@ -37,6 +39,10 @@ public class SampleDataLoadingFacadeTest {
 
     @Autowired
     public CourtDao courtDao;
+    
+    @Autowired
+    public UserDao userDao;
+    
 
     @Autowired
     public UserService userService;
@@ -51,7 +57,17 @@ public class SampleDataLoadingFacadeTest {
     public void createSampleData() throws IOException {
         log.debug("starting test");
         
-        Assert.assertTrue(bookingDao.findAll().size() > 0, "no bookings");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+      //  System.out.println(bookingDao.findAll().size());
+        
+      // System.out.println(courtDao.findAll().size());
+              	
+        System.out.println(userDao.findAll().size());
+
+
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+
+        //Assert.assertTrue(bookingDao.findAll().size() > 0, "no bookings");
 
         //Assert.assertTrue(courtDao.findAll().size() > 0, "no courts");
 
@@ -61,70 +77,3 @@ public class SampleDataLoadingFacadeTest {
     }
 
 }
-=======
-//package com.frenchies.tennisclub.sampledata;
-//
-//import java.io.IOException;
-//
-//import javax.persistence.EntityManager;
-//import javax.persistence.PersistenceContext;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.TestExecutionListeners;
-//import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-//import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-//import org.springframework.transaction.annotation.Transactional;
-//import org.testng.Assert;
-//import org.testng.annotations.Test;
-//
-//import com.frenchies.tennisclub.dao.BookingDao;
-//import com.frenchies.tennisclub.dao.CourtDao;
-//import com.frenchies.tennisclub.entity.User;
-//import com.frenchies.tennisclub.service.UserService;
-//
-///**
-// * Tests data loading.
-// *
-// * @author Meon Thomas 473449
-// */
-//@ContextConfiguration(classes = {TennisClubWithSampleDataConfiguration.class})
-//@TestExecutionListeners(TransactionalTestExecutionListener.class)
-//@Transactional
-//public class SampleDataLoadingFacadeTest extends AbstractTestNGSpringContextTests {
-//
-//    final static Logger log = LoggerFactory.getLogger(SampleDataLoadingFacadeTest.class);
-//
-//    @Autowired
-//    public BookingDao bookingDao;
-//
-//    @Autowired
-//    public CourtDao courtDao;
-//
-//    @Autowired
-//    public UserService userService;
-//
-//    @Autowired
-//    public SampleDataLoadingFacade sampleDataLoadingFacade;
-//
-//    @PersistenceContext
-//    private EntityManager em;
-//
-//    @Test
-//    public void createSampleData() throws IOException {
-//        log.debug("starting test");
-//        sampleDataLoadingFacade.loadData();
-//
-//        Assert.assertTrue(bookingDao.findAll().size() > 0, "no bookings");
-//
-//        Assert.assertTrue(courtDao.findAll().size() > 0, "no courts");
-//
-//        User admin = userService.getAllUsers().stream().filter(userService::isAdmin).findFirst().get();
-//        Assert.assertEquals(true, userService.authenticate(admin,"admin"));
-//        
-//    }
-//
-//}
->>>>>>> e5ea778890932b5877e295799a6f8f3f48e09e56
